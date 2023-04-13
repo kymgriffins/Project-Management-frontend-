b05dd93447f61ad85a6643fa9a34343f8bc49fae
kymgriffins <kimutaigriffins1960@gmail.com>
2023-04-13 08:26:19 +0300

    changed URL

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -1,2 +1,2 @@
-// export const URL = "https://web-production-f86e.up.railway.app/"
-export const URL = "http://127.0.0.1:8000/"
+export const URL = "https://web-production-f86e.up.railway.app/"
+// export const URL = "http://127.0.0.1:8000/"
